c6e21d2b8fb81190914a0477d13bb16c17f7f75e
AndresVillamil <bcl.evillamil@gmail.com>
2021-10-07 20:28:15 -0500

    Update Estudiante.cs

--- ProyectoGSA.App.Dominio/Entidades/Estudiante.cs
+++ ProyectoGSA.App.Dominio/Entidades/Estudiante.cs
@@ -1,29 +1,17 @@
 using System;
 namespace ProyectoGSA.App.Dominio.Entidades
 {
-    public class Estudiante 
+    public class Estudiante : Persona
     {
-        public Estudiante(int id,  string nombres, string nacimiento, Persona padre, Persona madre)
-        {
-            this.id = id;
-            this.nombres = nombres;
-            this.nacimiento = nacimiento;
-            this.padre = padre;
-            this.madre = madre;
-
-        }
-        public Estudiante()
-        {
-
-        }
+    
         
         public int id { get; set; }
-        public string nombres { set; get; }
+        /*public string nombres { set; get; }
         public string apellidos { set; get; }
         public string identificacion { set; get; }
         public string telefono { set; get; }
         public string mail { set; get; }
-        public string direccion { set; get; }
+        public string direccion { set; get; }*/
         public string nacimiento { get; set; }
         public Persona padre { get; set; }
         public Persona madre { get; set; }
