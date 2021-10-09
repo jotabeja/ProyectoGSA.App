85da169c754ecd3c4ffd0d13d1b0e5ef50178583
AndresVillamil <bcl.evillamil@gmail.com>
2021-10-08 22:43:38 -0500

    Integracion Dominio

--- ProyectoGSA.App.Presentacion/obj/Debug/netstandard2.0/RazorDeclaration/Pages/LiPersonas.razor.g.cs
+++ ProyectoGSA.App.Presentacion/obj/Debug/netstandard2.0/RazorDeclaration/Pages/LiPersonas.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace SGACOL.Shared
+namespace SGACOL.Pages
 {
     #line hidden
     using System;
@@ -47,23 +47,49 @@ using SGACOL.Shared;
 
 #line default
 #line hidden
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+#line 2 "C:\AppVers2\ProyectoSGA.App\ProyectoGSA.App.Presentacion\Pages\LiPersonas.razor"
+using ProyectoGSA.App.Dominio.Entidades;
+
+#line default
+#line hidden
+    [Microsoft.AspNetCore.Components.RouteAttribute("/lipersonas")]
+    public partial class LiPersonas : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#line 58 "C:\AppVers2\ProyectoSGA.App\ProyectoGSA.App.Presentacion\Shared\NavMenu.razor"
-       
-    bool collapseNavMenu = true;
+#line 50 "C:\AppVers2\ProyectoSGA.App\ProyectoGSA.App.Presentacion\Pages\LiPersonas.razor"
+ 
+    public List<Persona> ListaPersonas = new List<Persona>();
+    //public string id ="0";
 
-    string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+        protected override Task OnInitializedAsync()
+        {
+           Persona persona1 = new Persona();
+           persona1.id = 1;
+           persona1.nombres = "Andres";
+           persona1.apellidos = "Villamil";
+           /*persona1.identificacion = "79962837";
+           persona1.telefono = "2272783";
+           persona1.mail = "andres@gmail.com";
+           persona1.direccion = "Calle 78 # 120 -90";*/
+           Persona persona2 = new Persona();
+           persona2.id = 2;
+           persona2.nombres = "Diana";
+           persona2.apellidos = "Villamil";
+           /*persona2.identificacion = "79962837";
+           persona2.telefono = "2272783";
+           persona2.mail = "andres@gmail.com";
+           persona2.direccion = "Calle 78 # 120 -90"; */
 
-    void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
+            ListaPersonas.Add(persona1);
+            ListaPersonas.Add(persona2);
+
+            return base.OnInitializedAsync();
+        }
+
 
 #line default
 #line hidden
